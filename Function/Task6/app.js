d966add02c6f5b2f2a641a1d15b60316a218fb26
faleialina <august3@mail.ru>
2024-06-24 23:47:35 +0300

    task 6

--- Function/Task6/app.js
+++ Function/Task6/app.js
@@ -1,1 +1,8 @@
-//
+// 6. Напишите функцию, которая принимает массив чисел и возвращает среднее значение всех элементов.
+
+const averageValue = (arr) => {
+	const result = arr.reduce((el, sum) => sum += el / arr.length, 0)
+	return result
+}
+
+console.log(averageValue([1, 2, 3, 4, 5]));
