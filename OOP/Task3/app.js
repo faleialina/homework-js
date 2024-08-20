e9d284e71436c9a1b9766465b260fad5aad3f780
faleialina <august3@mail.ru>
2024-08-20 15:04:42 +0300

    oop task3

---
+++ OOP/Task3/app.js
@@ -0,0 +1,24 @@
+// 3. Реализуйте класс WordString, который будет иметь следующие методы: метод reverseString, переворачивающий строку, метод upperFirst, возвращающий строку, где первая буква заглавная и метод upperEvery, который делает заглавной первую букву каждого слова этой строки.
+
+class WordString {
+	constructor(str) {
+		this.str = str
+	}
+	reverseString() {
+		return this.str.split('').reverse().join('')
+	}
+	upperFirst() {
+		return this.str[0].toUpperCase() + this.str.slice(1)
+	}
+	upperEvery() {
+		return this.str
+			.split(' ')
+			.map(elem => elem[0].toUpperCase() + elem.slice(1))
+			.join(' ')
+	}
+}
+
+const wordString = new WordString('hello world')
+console.log(wordString.reverseString())
+console.log(wordString.upperFirst())
+console.log(wordString.upperEvery())
