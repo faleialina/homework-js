ee15181516ce7c9c1c981021f36c5f9d6656f72c
faleialina <august3@mail.ru>
2024-08-23 18:47:51 +0300

    oop task 8-9

--- OOP/Task8/app.js
+++ OOP/Task8/app.js
@@ -1,2 +1,10 @@
 // 	8. Реализуйте класс Validator. У него будет метод isEmail параметром принимает строку и проверяет, является ли она корректным емейлом или нет. Если является - возвращает true, если не является то false.
 
+class Validator {
+	isEmail = str => {
+		return /^[a-z0-9_\.-]+@[a-z]+\.[a-z]{2,3}$/gm.test(str) ? true : false
+	}
+}
+
+const validator = new Validator()
+console.log(validator.isEmail('august3@mail.ru'))
--- OOP/Task9/app.js
+++ OOP/Task9/app.js
@@ -1,3 +1,15 @@
 // 	9. Реализуйте класс для пересчета временного интервала, заданного в минутах, в величину, выраженнуювчасахиминутах.
 // 	Входные: 150 минут →Результат: 2 часа 30 минут
 // 	Входные: 240 минут→Результат:4 часа 0минут
+class Time {
+	constructor(minutes) {
+		this.minutes = minutes
+	}
+	conversion = () => {
+		const hours = Math.floor(this.minutes / 60);
+    const remainingMinutes = this.minutes % 60;
+		return `${hours} часа ${remainingMinutes} минут`;
+	}
+}
+const time = new Time(240)
+console.log(time.conversion())
