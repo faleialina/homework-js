942d8367554251bde4eac49cc0c84ff2d1e1cbdb
faleialina <august3@mail.ru>
2024-08-24 15:06:20 +0300

    oop task 16

--- OOP/Task16/app.js
+++ OOP/Task16/app.js
@@ -3,3 +3,22 @@
 //  Входные: Employee("Alice", "Developer", 5000) → Результат: 60000
 //  Входные: Employee("Bob", "Manager", 7000) → Результат: 84000
 //  Входные: Employee("Charlie", "Designer", 4000) → Результат: 48000
+class Person {
+	name;
+}
+
+class Employee extends Person {
+			position;
+			monthly_salary;
+
+	getAnnualSalary() {
+			return `Результат: ${this.name}, ${this.position}, ${this.monthly_salary * 12}`;
+	}
+}
+
+const employee = new Employee();
+employee.name = "Alice";
+employee.position = "Developer";
+employee.monthly_salary = 5000;
+
+console.log(employee.getAnnualSalary());
