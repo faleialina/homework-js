6cb6a0101001f0f63f145ba4bb5d0b8d17b5f56a
faleialina <august3@mail.ru>
2024-08-25 20:45:43 +0300

    oop task 17

--- OOP/Task17/app.js
+++ OOP/Task17/app.js
@@ -1,6 +1,20 @@
 //  8. Создайте класс Vehicle с атрибутом speed. На основе этого класса создайте класс Car, добавив
-//  атрибут brand и метод getInfo(), который выводит информацию о машине в формате "Brand: 
+//  атрибут brand и метод getInfo(), который выводит информацию о машине в формате "Brand:
 // <brand>, Speed: <speed>".
 //  Входные: Car("Toyota", 120).getInfo() → Результат: "Brand: Toyota, Speed: 120"
 //  Входные: Car("Ford", 150).getInfo() → Результат: "Brand: Ford, Speed: 150"
 //  Входные: Car("BMW", 180).getInfo() → Результат: "Brand: BMW, Speed: 180«
+
+class Vehicle {
+	speed
+}
+class Car extends Vehicle {
+	brand
+	getInfo() {
+		return `"Brand: ${this.brand}, Speed: ${this.speed}"`
+	}
+}
+const car = new Car()
+car.brand = 'Ford'
+car.speed = 150
+console.log(car.getInfo())
