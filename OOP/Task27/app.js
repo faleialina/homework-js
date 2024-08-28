a772788086a17028afcf6abab2f54164f9259c8e
faleialina <august3@mail.ru>
2024-08-28 23:37:43 +0300

    oop task 27

--- OOP/Task27/app.js
+++ OOP/Task27/app.js
@@ -1,4 +1,3 @@
-
 // 9. Создайте родительский класс RandomNumberArrayGenerator, который будет генерировать массив
 // случайных чисел с помощью метода generateArray(). Затем создайте дочерний класс MultipleFilter,
 // который переопределяет метод generateArray() и оставляет в массиве только те числа, которые
@@ -7,3 +6,22 @@
 // случайные)
 // Входные: new RandomNumberArrayGenerator().generateArray() → Результат: [13, 21, 8, 9, 14, 27] (может быть
 // другим, так как числа случайные)
+
+class RandomNumberArrayGenerator {
+	generateArray() {
+		const array = []
+		for (let i = 0; i < 10; i++) {
+			array.push(Math.floor(Math.random() * 100))
+		}
+		return array
+	}
+}
+
+class MultipleFilter extends RandomNumberArrayGenerator {
+	generateArray() {
+		return super.generateArray().filter(el => el % 3 == 0 || el % 7 == 0)
+	}
+}
+
+const multipleFilter = new MultipleFilter()
+console.log(multipleFilter.generateArray())
